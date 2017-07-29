21d276d8d5601093c3e21bf075b45080fae99640
linuxautomations <raghukx@gmail.com>
2017-07-29 08:40:48 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://linuxautomations@bitbucket.org/linuxautomations/gitkeys.git
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+mkdir -p ~/bin
+cp gitkeys/Git ~/bin
+cp gitkeys/gitset ~/bin
+chmod +x ~/bin/Git ~/bin/gitset
+
+cd gitkeys/SSH 
+cp config* carreerit indexit linuxauto ~/.ssh
