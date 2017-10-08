22eb702c9d98965a734d276319cb369cfaacd0b8
Raghu K <raghuk.vit@gmail.com>
2017-10-08 15:12:44 +0000

    Updating gitkeys

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 yum install git -y &>/dev/null
+PWD=$(pwd)
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
 git clone https://linuxautomations@bitbucket.org/linuxautomations/gitkeys.git &>/dev/null
 if [ $? -ne 0 ]; then
 	echo "Unable to download repository"
 	exit 1
 fi
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
 mkdir -p ~/bin
 cp gitkeys/Git ~/bin
 cp gitkeys/gitset ~/bin
@@ -18,4 +20,5 @@ cd gitkeys/SSH
 cp config* carreerit indexit linuxauto ~/.ssh
 cd ~/.ssh
 chmod 600 *
+rm -rf $PWD/gitkeys
 gitset cit
